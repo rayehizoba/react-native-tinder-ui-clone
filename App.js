83fc5ea3b05eafccea69720eb9120d8aa81f2e40
rayehizoba <rayehizoba@gmail.com>
2018-05-04 11:45:44 +0100

    Feat: Add PanResponder

--- App.js
+++ App.js
@@ -1,13 +1,64 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Image, Animated, PanResponder } from 'react-native';
+
+const SCREEN_HEIGHT = Dimensions.get('window').height;
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const Users = [
+  {id: "1", uri: require('./assets/image1.jpg')},
+  {id: "2", uri: require('./assets/image2.jpg')},
+  {id: "3", uri: require('./assets/image3.jpg')},
+  {id: "4", uri: require('./assets/image4.jpg')},
+  {id: "5", uri: require('./assets/image5.jpg')},
+]
 
 export default class App extends React.Component {
+
+  constructor() {
+    super()
+
+    this.position = new Animated.ValueXY()
+    this.state = {
+      currentIndex: 0,
+    }
+  }
+
+  componentWillMount() {
+    this.PanResponder = PanResponder.create({
+      onStartShouldSetPanResponder:(evt,gestureState) => true,
+      onPanResponderMove:(evt,gestureState) => {
+
+        this.position.setValue({x: gestureState.dx, y: gestureState.dy})
+      },
+      onPanResponderRelease:(evt,gestureState) => {
+
+      }
+    })
+  }
+
+  renderUsers =  () => {
+    return Users.map((item, i) => {
+
+      return (
+        <Animated.View key={item.id} {...this.PanResponder.panHandlers}
+          style={[{transform: this.position.getTranslateTransform()}, {height:SCREEN_HEIGHT-120, width:SCREEN_WIDTH, padding: 10, position: 'absolute' }]} >
+          <Image style={{flex:1, height:null, width:null, resizeMode:'cover', borderRadius: 20}} source={item.uri} />
+        </Animated.View>
+      )
+    }).reverse();
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{flex: 1}} >
+        <View style={{height: 60}} >
+
+        </View>
+        <View style={{flex: 1}} >
+          {this.renderUsers()}
+        </View>
+        <View style={{height: 60}} >
+
+        </View>
       </View>
     );
   }
